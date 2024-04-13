b504eaecaaef7821aef94b40798f1fd888041b97
kb603 <kojoaddobaffour@gmail.com>
2024-04-13 16:01:17 +0000

    updates

--- part 2/script.js
+++ part 2/script.js
@@ -1,5 +1,6 @@
 "use strict"; 
 
+/*
 function logger() {
     console.log("My name is Kojo");
 }
@@ -17,3 +18,49 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor (2, 3);
 console.log(appleOrangeJuice);
+*/
+
+//Function Declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(2004);
+
+//Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(2004);
+
+console.log(age1, age2)
+
+//arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(2004);
+console.log(age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 60 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years.`
+}
+console.log(yearsUntilRetirement(2004, "Kojo"));
+
+
+
+const cutPieces = function(fruit) {
+    return fruit * 4;
+};
+
+const fruitProcessor = function fruitProcessor(apples, oranges) {
+
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
