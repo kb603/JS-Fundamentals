b10967cb49dbbeeed33c51c5bd61b59999cec2f5
kb603 <kojoaddobaffour@gmail.com>
2024-04-22 22:06:07 +0000

    updates

--- modal/script.js
+++ modal/script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+console.log(btnShowModal);
+
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", showModal);
+
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+
+  btnCloseModal.addEventListener("click", closeModal);
+
+  overlay.addEventListener("click", closeModal);
+}
