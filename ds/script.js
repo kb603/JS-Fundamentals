6b2aeb4080f0dc35d9b9debdd43d437358b256a7
kb603 <kojoaddobaffour@gmail.com>
2024-04-27 15:05:23 +0000

    updates

--- ds/script.js
+++ ds/script.js
@@ -40,9 +40,45 @@ const restaurant = {
       `Here is your deleicious pasta with ${ing1},  ${ing2},  ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
-// SPREAD OPERATOR
+// SPREAD,because on the RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+//REST, because on the left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, ...otherFood);
+
+// Objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza("tomatoes", "peproni", "sausage", "hot sauce");
 /*
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
