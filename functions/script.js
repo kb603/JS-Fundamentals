6d8b6fe7619b5ef5a66250ac525cd261c766d20f
kb603 <kojoaddobaffour@gmail.com>
2024-04-29 18:24:53 +0000

    updates

--- functions/script.js
+++ functions/script.js
@@ -1,4 +1,5 @@
 "use strict";
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -48,3 +49,85 @@ const newPassport = function (person) {
 newPassport(kojo);
 checkIn(flight, kojo);
 console.log(newPassport);
+*/
+
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher Order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer(`JavaScript is the best!`, upperFirstWord);
+*/
+
+/*
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Steven");
+
+// Arrow Function
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+greetArr("hi")("kojo ");
+*/
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode} ${flightNum}, name` });
+  },
+};
+
+lufthansa.book(2384, "John Smith");
+lufthansa.book(3478, "Jane Smith");
+console.log(lufthansa);
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Call Method
+book.call(eurowings, 23, "Sarah Williams");
+console.log(eurowings);
+book.call(lufthansa, 239, "Mary Cooper");
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Airline",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 583, "Mary Cooper");
+console.log(swiss);
+
+// Apply Method
+const flightData = [583, "George Cooper"];
+book.apply(swiss, flightData);
+console.log(flightData);
