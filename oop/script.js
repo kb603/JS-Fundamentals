accab7d5cae31b26f4ead976f1fe9c3c02caaaf6
kb603 <kojoaddobaffour@gmail.com>
2024-05-07 22:04:37 +0000

    updates

--- oop/script.js
+++ oop/script.js
@@ -1,1 +1,42 @@
-'use strict';
+"use strict";
+
+/*
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person("Jonas", 1971);
+console.log(jonas);
+
+const matilda = new Person("Matilda", 2017);
+const jake = new Person("Jake", 1975);
+
+console.log(matilda, jake);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+*/
+
+/*
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const bmw = new Car("Bmw", 120);
+const mercedes = new Car("Mercedes", 95);
+
+Car.prototype.accelerate = function (speed) {
+  this.speed += 5;
+};
+
+bmw.accelerate();
+console.log(bmw);
+console.log(mercedes);
+*/
